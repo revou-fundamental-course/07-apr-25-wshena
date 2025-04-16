44fc8ab6c3a90437bd04a2a8156d11bc25f91b11
poppywydy <adeliasekar7@gmail.com>
2025-04-16 16:06:29 +0700

    add portofolio section.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,58 @@
+// global var
+var formData; 
+console.log('Data Form:', formData);
+// alert
+const alert = document.querySelector('#alert');
+const alertContainer = document.querySelector('.alert-container');
+
+const autoRemoveAlert = (timeout) => {
+  setTimeout(() => {
+    const computedTransform = getComputedStyle(alertContainer).transform;
+    if (computedTransform.includes('0, 350)')) { // Contoh pengecekan sederhana
+      alertContainer.style.transform = 'translateY(-1000px)';
+    }
+  }, timeout);
+}
+
+// form
+const form = document.querySelector('#form');
+const firstNameInput = document.querySelector('#first-name');
+const lastNameInput = document.querySelector('#last-name');
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
+
+// handle form submit function
+const handleSubmitForm = (e) => {
+  e.preventDefault();
+
+  const firstName = firstNameInput.value
+  const lastName = lastNameInput.value
+  const email = emailInput.value
+  const message = messageInput.value
+
+  if (!firstName || !lastName || !email || !message) {
+    alert.innerText = 'Mohon isi semua field pada form';
+    alertContainer.style.transform = 'translateY(350px)'
+    autoRemoveAlert(4000);
+    return;
+  }
+
+  formData = {
+    firstName,
+    lastName,
+    email,
+    message
+  };
+
+  form.reset();
+}
+
+// mobile nav menu
 const mobileNav = document.querySelector('#mobile-nav')
 const mobileNavOpenButton = document.querySelector('#toggle-menu-open');
 const mobileNavCloseButton = document.querySelector('#toggle-menu-close');
 
+// mobile nav function
 const toggleOpenMobileNav = () => {
   mobileNavOpenButton.addEventListener('click', () => {
     mobileNav.classList.add('mobile-nav-active');
@@ -17,3 +68,4 @@ const toggleCloseMobileNav = () => {
 // function run
 toggleOpenMobileNav();
 toggleCloseMobileNav();
+form.addEventListener('submit', handleSubmitForm);
